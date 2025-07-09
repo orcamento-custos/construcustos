716ba68f1b072f53c1d519073f09bf9de5019a0d
orcamento-custos <taciimendes@gmail.com>
2025-07-09 15:39:49 -0300

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Construcustos</title>
+      </Head>
+      <main>
+        <h1>Simulador de Orçamento de Obra</h1>
+        <p>Bem-vindo ao Construcustos! Em breve seu orçamento estará funcionando aqui.</p>
+      </main>
+    </div>
+  );
+}
